92b11f4dc26f6af58131f52eaa3211e2fedf5c1f
Vinicius Ribeiro <vayne99@gmail.com>
2023-01-25 11:32:59 -0300

    fix: authentication with hashed password

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
@@ -10,7 +11,8 @@ export class AuthService {
 
     async validateUser(email: string, password: string): Promise<any> {
         const user = await this.usersService.user({ email })
-        if (user && user.password === password) {
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (user && isPasswordCorrect) {
             const { password, ...result } = user
             return result
         }
@@ -20,6 +22,7 @@ export class AuthService {
     async login(user: any) {
         const payload = { email: user.email, sub: user.id }
         return {
+            user,
             access_token: this.jwtService.sign(payload),
         }
     }
