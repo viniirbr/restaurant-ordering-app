24412574beda3d8ac6e22ffb0433ab52e0f2fa98
Vinicius Ribeiro <vayne99@gmail.com>
2023-01-25 13:19:38 -0300

    feat: protect routes with bearer token

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport'
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -12,6 +12,12 @@ export class AuthController {
     @Post('login')
     async login(@Request() req) {
         return this.authService.login(req.user)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    async profile(@Request() req) {
+        return req.user
     }
     
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { email: user.email, sub: user.id }
+        const payload = { email: user.email }
         return {
             user,
             access_token: this.jwtService.sign(payload),
